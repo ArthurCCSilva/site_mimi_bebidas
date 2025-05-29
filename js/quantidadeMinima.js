5d5118006d0359fec627c1218530fbf755127866
ArthurCCSilva <arthurccs2013@gmail.com>
2025-05-28 23:58:08 -0300

    lista de quantidade minima
    atualizado nomes

--- js/quantidadeMinima.js
+++ js/quantidadeMinima.js
@@ -1,6 +1,23 @@
     // 📌 Quantidades mínimas por produto
     const quantidadesMinimas = {
-        11: 2, 14: 6, 15: 6, 32: 6, 34: 6, 36: 6,
-        38: 6, 39: 2, 49: 6, 60: 6, 68: 3, 86: 2,
-        92: 2, 93: 2, 94: 2, 97:3, 113: 3, 115: 3, 123: 6, 125: 3
+        11: 2,//Cigarro K-Lint Silver c/20 unidades
+        14: 6,//Cerveja Petra Lata 350ml
+        15: 6,//Cerveja Skol Lata 350ml
+        32: 6,//Cerveja Devassa Lata 350ml
+        34: 6,//Cerveja Crystal Lata 350ml
+        36: 6,//Cerveja Bohemia Lata 350ml
+        38: 6,//Cerveja Itaipava Lata 350ml
+        39: 2,//Vinho Vale Real 900ml
+        49: 6,//Cerveja Brahma Chopp Lata 350ml
+        60: 6,//Cerveja Brahma Duplo Malte Lata 350ml
+        68: 3,//Cabaré ICE Limão 275ml
+        86: 2,//Cigarro Gift c/20 unidades
+        92: 2,//Cigarro Rothmans Blue Global c/20 unidades
+        93: 2,//Cigarro Rothmans Classic Blue (Branco) c/20 unidades
+        94: 2,//Cigarro Rothmans Blue c/20 unidades
+        97: 3,//Cerveja Spaten Long Neck 355ml
+        113: 3,//Skol Beats Senses Long Neck 269ml
+        115: 3,//Skol Beats Red Mix Long Neck 269ml
+        123: 6,//Spaten Lata 350ml
+        125: 3//Smirnoff Ice Long Neck 275ml
     };
